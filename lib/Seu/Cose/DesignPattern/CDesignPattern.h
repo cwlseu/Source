2bd04df2d9116e30e6410f08c7a0d2ecd31975a4
cwlseu <caowenlong92@gmail.com>
2014-11-26 23:42:34 +0800

    DesignPattern namespace header file

---
+++ lib/Seu/Cose/DesignPattern/CDesignPattern.h
@@ -0,0 +1,26 @@
+#ifndef __SEU_COSE_CDESIGNPATTERN_H_
+#define __SEU_COSE_CDESIGNPATTERN_H_
+namespace Seu{
+	namespace Cose{
+		namespace DesignPattern
+		{
+			class AbstractFactory;
+			class Proxy;
+			class Observer;
+			class Builder;
+			class Prototype;
+			class Singleton;
+			class Adapter;
+			class Decorator;
+			class Facade;
+			class FlyweightFactory;
+			class Component;
+			class SuccessHandle;
+			class Invoker;
+			class AbstractExpression;
+			class State;
+			class Strategy;
+		}
+	}
+}
+#endif
